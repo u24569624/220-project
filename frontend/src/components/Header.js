70a3900ba30a49992c19888edc31e25cc25fa867
u24569624 <u24569624@tuks.co.za>
2025-09-04 10:47:41 +0200

    fixed sidebar error

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {useState} from 'react';
-import Sidebar from '../components/sidebar.js'
+import Sidebar from './sidebar.js'
 import '../styles/Header.css';
 import '../styles/global.css';
 
